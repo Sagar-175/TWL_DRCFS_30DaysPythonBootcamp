914d73d8ab7d3fe2bf7ab5703978a78bc1046c19
Sagar Gosai <sagargosai175@gmail.com>
2022-12-09 19:45:19 +0545

    friday assignment

---
+++ Week 1/Assignments/rock.py
@@ -0,0 +1,15 @@
+# Rock paper scissors
+import random
+print("Welcome!!! Let's play a game")
+options = ['rock', 'paper', 'scissors']
+player2 = random(options)
+for i in range(5):
+    player1 = input("Choose your answer:")
+    if player1 == player2:
+        print('The game is tie. No one wins')
+    elif player1 == 'rock' and player2 == 'scissors':
+        print('Player 1 wins')
+    elif player1 == 'paper' and player2 == 'rock':
+        print('Player 1 wins')
+    elif player1 == 'scissors' and player2 == 'paper':
+        print('Player 1 wins')
