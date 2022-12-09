820b3a069dcf5fc877021c2bd92de552a5a21095
Sagar Gosai <sagargosai175@gmail.com>
2022-12-09 20:15:37 +0545

    changes

--- Week 1/Assignments/rock.py
+++ Week 1/Assignments/rock.py
@@ -2,6 +2,7 @@
 import random
 print("Welcome!!! Let's play a game")
 options = ['rock', 'paper', 'scissors']
+print(options)
 player2 = random.choice(options)
 print(player2)
 for i in range(5):
@@ -10,7 +11,13 @@ for i in range(5):
         print('The game is tie. No one wins')
     elif player1 == 'rock' and player2 == 'scissors':
         print('Player 1 wins')
+    elif player1 == 'rock' and player2 == 'paper':
+        print('Player 2 wins')
     elif player1 == 'paper' and player2 == 'rock':
         print('Player 1 wins')
+    elif player1 == 'paper' and player2 == 'scissors':
+        print('Player 2 wins')
     elif player1 == 'scissors' and player2 == 'paper':
         print('Player 1 wins')
+    elif player1 == 'scissors' and player2 == 'rock':
+        print('Player 2 wins')
